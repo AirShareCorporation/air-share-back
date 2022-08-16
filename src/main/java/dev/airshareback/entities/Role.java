a198d31d57d1af5f2f84ca18887c1bc5e7697c39
GabagoolOverHere <vicmartindev@gmail.com>
2022-08-16 15:37:51 +0200

    entite user, role, statut et statut de moderation

---
+++ src/main/java/dev/airshareback/entities/Role.java
@@ -0,0 +1,31 @@
+package dev.airshareback.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
