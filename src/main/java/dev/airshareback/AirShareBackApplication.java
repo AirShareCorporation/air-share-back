a9149a52e964df5c1eb035ed5c817f92c5512471
GabagoolOverHere <vicmartindev@gmail.com>
2022-08-16 15:07:29 +0200

    premier commit

---
+++ src/main/java/dev/airshareback/AirShareBackApplication.java
@@ -0,0 +1,13 @@
+package dev.airshareback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirShareBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirShareBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/airshareback/AirShareBackApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.airshareback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirShareBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
