32e573d7d15017f3a10b9ff7828e13b52b3bec22
DavidB <david.b@hotmail.fr>
2022-08-16 16:21:34 +0200

    Entités: AirData, CensusData, Unit

---
+++ src/main/java/dev/airshareback/entities/CensusData.java
@@ -0,0 +1,36 @@
+package dev.airshareback.entities;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "census_data")
+public class CensusData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private Date datetime;
+
+    @Column(nullable = false)
+    private Float data;
+
+    public Float getData() {
+        return data;
+    }
+
+    public Date getDatetime() {
+        return datetime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
