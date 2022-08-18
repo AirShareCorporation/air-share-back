8672c08114107287f3995cb205391635fc02d431
GabagoolOverHere <vicmartindev@gmail.com>
2022-08-18 17:32:00 +0200

    creation meteo data

--- src/main/java/dev/airshareback/repositories/MeteoDataRepository.java
+++ src/main/java/dev/airshareback/repositories/MeteoDataRepository.java
@@ -3,5 +3,8 @@ package dev.airshareback.repositories;
 import dev.airshareback.entities.MeteoData;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MeteoDataRepository extends JpaRepository<MeteoData, Integer> {
+    public List<MeteoData> findByCityOrderByDateTimeDesc(Long id);
 }
