d45734868b92e42818b5486de131e29aded7236b
GabagoolOverHere <vicmartindev@gmail.com>
2022-08-21 14:31:32 +0200

    fix census repo

--- src/main/java/dev/airshareback/repositories/CensusDataRepository.java
+++ src/main/java/dev/airshareback/repositories/CensusDataRepository.java
@@ -1,7 +1,9 @@
 package dev.airshareback.repositories;
 
 import dev.airshareback.entities.CensusData;
+import dev.airshareback.entities.City;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CensusDataRepository extends JpaRepository<CensusData, Integer> {
+    CensusData findByCity(City city);
 }
